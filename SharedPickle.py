e670247f70dbf37ea706ae13ed47d472c5a3f8b2
odolan-gradient <odolan@morningstarco.com>
2024-08-15 15:15:56 -0700

    Please work

--- SharedPickle.py
+++ SharedPickle.py
@@ -9,44 +9,64 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload, MediaIoBaseUpload
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Constants
 DIRECTORY_YEAR = "2024"
 PICKLE_NAME = f"{DIRECTORY_YEAR}_pickle_test.pickle"
 
-# Google Cloud Storage setup
-storage_client = storage.Client()
-bucket_name = 'gradient-pickle'
-bucket = storage_client.bucket(bucket_name)
-cloud_pickle_path = f"Pickle/{PICKLE_NAME}"
-
 SHARED_DRIVE_ID = '0ACxUDm7mZyTVUk9PVA'
 PICKLE_FILE_ID = '1h9fu1mZa9pzQLDOIjEpejBz8zKpbMtyG'
-CREDENTIALS_FILE = r'C:\Users\odolan\PycharmProjects\SlackBot\client_secret_creds.json'
+# CREDENTIALS_FILE = r'C:\Users\odolan\PycharmProjects\SlackBot\client_secret_creds.json'
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
-CREDENTIALS_FILE = 'rich-meridian-430023-j1-6c518e24db10.json'
 
 def get_drive_service():
-    credentials = None
+    credentials_info = {
+          "type": "service_account",
+          "project_id": "rich-meridian-430023-j1",
+          "private_key_id": "66cbc6d4279f474fe9c83e1ada738099fec5b251",
+          "private_key": "-----BEGIN PRIVATE "
+                         "KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCpP1qKCDDGQcjo"
+                         "\n83Wub6pu8g6uwtGU1vKPqhugPwo8kCqA3Rykynq4rHPGVDeHAPo/Iu+t90RyCvMd\nvzNh0SXkKKWqSh9HJvP1"
+                         "/Taip3cS7YdNTiUxC/us5r00g+8npOaiThBnAh+FXKpd\nn/ASutz5yZ21AF2yqMrhvnIutFcf/E4OZn"
+                         "+MgHQN1dpcD7+P0jgX9I6QZmpNG1ig\nKhHauVf5ry/35KPConycYgFaUDOnMsif/CeYRoN6L7ZJ9kUCrkUxZ"
+                         "+tnZaUJ3Puw\noMLf6KR6YY7HOXQTSaUZAjYkxrv6aGnYC37cMx47WkmvpwdKZJYH4hzuA1xnwzn8"
+                         "\nwy6w35ilAgMBAAECggEAAm+YPsDX7N6RBPNOGAzg49hliDPjHtSKKLGu1Jtbqxv7\nFKA6E5AbfJF02B"
+                         "+pre6Aa4y17OfQayDHt3+jPm7rb/F60uzeruA7Zii3Ete8sb/L\n8PulMuPEg0xN4FXeyRAJRsA/YbAo4ns"
+                         "/M3pEEwzv9cNmWu7Oqm3d/6pFS/FKCLqL\norSN0wLW2dNZ9FasFmLuSAGOGxFyW"
+                         "+zzYQl5Hlybnuk0mIV8WwxhpbCRCy3NgyM3\ntU0pYoaBwA3Z/zY"
+                         "+G91t7G66shKi5u7EZtiI7VGSMrkSY3zBUeA7gAaPEw1Lvm8Y"
+                         "\nEWSLLjefNXJW8cTtgYh1GGHjp11tSLlGE3VWTORAcQKBgQDltAB9fEuMsQ7/l+7Z"
+                         "\n0WI7a41f3EgKNDw2jjaxDTfq49VTEyQmpuTiVbbSmRRaKOWtSmoSaqwLdLbx10Om\nc1YOh1"
+                         "/kiNpaaFqJPvGQjHtgn8fcYhAeCXUiUzctnYRLSgx3dvbGxpCoZ13VVK21"
+                         "\n8EIrOSyTsaeq8S5BzmyxtXBC9QKBgQC8n432yE7+7f1l8Z8v9Nk+2Edl5oRxOuqV\nwjLYD5kttTYR5B5"
+                         "+uj4xsvfKVlfR6QkYNT3DBzq5jGYYOejhbm08msqWW+jdPLSi\nLh9Y55zkPZa/vuOAnOp1z52L21yM2jL27cXJ"
+                         "/nu82VVM55hI4aRQ7ppi1MvgpXNw\nE+ZUNbRQ8QKBgH4OGxrCJD+wRvfS6/vS4SKUsj"
+                         "/CBjK7WbPitXbSNzaLE12Eqpkf\ni4n92deWtEmKGgjQRoeWzJV41pC/PlvQ/Y/5kJE83P8yN0UMKsrVnTt4U9jIY"
+                         "+nn\n7MUKf8Rjpd8fYtoIigKpo2cXWrIgxzeKAvXvaVwf6VBxDJ6GZrXbSSElAoGAPvKE"
+                         "\nXvokGsFzkkTbWha9NVLaKPCP/HWr+cRwUViLRwy1ea0GXEZtIQrX1MeR0TSS22hR\nLzfHakqne6g"
+                         "/xpOiktoZh6ougT6UDZeU0Iei/SxslZrvs2kqeZyKuDTBoyPiZDOf\nkTSDONfStrKHSLM8seGe1iKr01GDv8B0Wl"
+                         "/9yBECgYEA0ivupeVyehM1dRqJqxZy"
+                         "\nQyn7QKSTP4Ulaw3NiLYx4mQiCgQKpdgrGcB7g8S8cKmlgZ9aXhZA1SdnM3Bm5fz8\n8CwVNQHZgzjq+ZbTt7"
+                         "/WzRLiGm5KLtbx2QRk7jljL/DQ5pD76pddq7QwV20spUtQ\nt6QBEuRN3lcSlSec3Sqpt5g=\n-----END PRIVATE "
+                         "KEY-----\n",
+          "client_email": "slack-bot-service-account@rich-meridian-430023-j1.iam.gserviceaccount.com",
+          "client_id": "104920020437657199869",
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/slack-bot-service-account%40rich"
+                                  "-meridian-430023-j1.iam.gserviceaccount.com",
+          "universe_domain": "googleapis.com"
+    }
 
-    # Check if the credentials file exists
-    if os.path.exists(CREDENTIALS_FILE):
-        # Load credentials from the JSON file
-        credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=SCOPES)
-    else:
-        # Get the JSON string from the environment variable
-        credentials_json = os.environ.get("GOOGLE_CREDENTIALS_JSON")
-        if not credentials_json:
-            raise ValueError("GOOGLE_CREDENTIALS_JSON environment variable not set")
+    credentials = service_account.Credentials.from_service_account_info(
+        credentials_info, scopes=['https://www.googleapis.com/auth/drive'])
 
-        # Load the credentials as a JSON object
-        credentials_info = json.loads(credentials_json)
-
-        # Create a service account credentials object
-        credentials = service_account.Credentials.from_service_account_info(credentials_info, scopes=SCOPES)
-
-    # Build the Drive service
     service = build('drive', 'v3', credentials=credentials)
     return service
 
